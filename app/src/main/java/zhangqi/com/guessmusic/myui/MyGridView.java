d4d661347cb04cab361778379dbca5523949ee85
Administrator <435559866@qq.com>
2016-04-04 20:50:23 +0800

    测试

--- app/src/main/java/zhangqi/com/guessmusic/myui/MyGridView.java
+++ app/src/main/java/zhangqi/com/guessmusic/myui/MyGridView.java
@@ -5,6 +5,7 @@ import android.util.AttributeSet;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.Button;
 import android.widget.GridView;
 
 import java.util.ArrayList;
@@ -48,12 +49,14 @@ public class MyGridView extends GridView {
         }
 
         @Override
-        public View getView(int position, View v, ViewGroup p) {
+        public View getView(int pos, View v, ViewGroup p) {
             WordButton holder;
             if(v==null){
                 v= Util.getView(mContext, R.layout.self_ui_gridview_item);
 
-                holder = mArrayList.get(position);
+                holder = mArrayList.get(pos);
+                holder.mIndex = pos;
+                holder.mViewButton = (Button) findViewById(R.id.item_btn);
             }
             return null;
         }
